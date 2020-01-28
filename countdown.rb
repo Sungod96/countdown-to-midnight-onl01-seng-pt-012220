28cc7e6f84a7d02c5c862b5c77941ca700212ea7
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-01-28 07:59:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
 
 def countdown
+  countdown = 1
+  while countdown < 10 
+  puts "#{countdown} seconds left until the New Year!"
+  countdown -= 1
 end
+
+puts "HAPPY NEW YEAR!"
+
+def countdown_with_sleep
+  countdown_with_sleep = 1
+  sleep.1 until countdown_with_sleep == 10
+  puts "#{countdown_with_sleep} seconds until the New Year!"
+  countdown_with_sleep -= 1 
+end
+
+puts "HAPPY NEW YEAR!"
+  
+  
